077805dd9a00e4aaa05d3894eece218b164e343c
honeypunch97 <bkws04211@naver.com>
2023-07-11 21:43:44 +0900

    add: initial style

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,19 @@
 import React, { memo } from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import { Outlet } from 'react-router-dom';
+import { MainContainer, WrapContainer } from '../styled/GamilyStyle';
 
 const Layout = memo(() => {
-   return <div></div>;
+   return (
+      <WrapContainer>
+         <Header />
+         <MainContainer>
+            <Outlet />
+         </MainContainer>
+         <Footer />
+      </WrapContainer>
+   );
 });
 
 export default Layout;
