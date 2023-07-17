2c12f6799f7e19e87de379327b45b8785fac4cde
honeypunch97 <bkws04211@naver.com>
2023-07-17 16:19:25 +0900

    fix: merge in store index

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -3,15 +3,9 @@ import adoptionsR from './modules/adoptionSlice';
 import storeR from './modules/storeSlice';
 import quizR from './modules/quizSlice';
 import knowledgeR from './modules/knowledgeSlice';
-<<<<<<< HEAD
 import authR from './modules/authSlice';
-
-export const store = configureStore({
-   reducer: { adoptionsR, storeR, quizR, knowledgeR, authR },
-=======
 import qnaR from './modules/qnaSlice';
 
 export const store = configureStore({
-   reducer: { adoptionsR, storeR, quizR, knowledgeR, qnaR },
->>>>>>> 3bebbc251494e977838cfc38531d3d151803d0ae
+   reducer: { adoptionsR, storeR, quizR, knowledgeR, authR, qnaR },
 });
