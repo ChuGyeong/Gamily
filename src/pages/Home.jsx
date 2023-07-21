6b1b111755b43a3074a4591b72dcef44ef0a6eac
honeypunch97 <bkws04211@naver.com>
2023-07-21 16:21:35 +0900

    add: react-intersection-observer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,10 @@
 import React, { memo } from 'react';
 import Visual from '../components/home/Visual';
 import VisualText from '../components/home/VisualText';
+import ExpandingSection from '../components/home/ExpandingSection';
 
 const Home = memo(() => {
-   return (
-      <>
-         <VisualText />
-      </>
-   );
+   return <>{/* <VisualText /> */}</>;
 });
 
 export default Home;
