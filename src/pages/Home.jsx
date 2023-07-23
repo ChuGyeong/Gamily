c9237eedd5df1cae9a4674f288e9962185c5a2fa
ChuGyeong <unkns@naver.com>
2023-07-23 17:45:51 +0900

    update: home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React, { memo } from 'react';
 import Visual from '../components/home/Visual';
 import VisualText from '../components/home/VisualText';
+import VisualSlide from '../components/home/VisualSlide';
 
 const Home = memo(() => {
    return (
       <>
          <VisualText />
+         <VisualSlide />
       </>
    );
 });
