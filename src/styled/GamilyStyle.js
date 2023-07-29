762c16758a19ca12df8391b53a3d21c7fa28d5fd
honeypunch97 <bkws04211@naver.com>
2023-07-29 21:57:19 +0900

    fix: style

--- src/styled/GamilyStyle.js
+++ src/styled/GamilyStyle.js
@@ -3434,3 +3434,29 @@ export const MenuCrossContainer = styled.div`
       }
    }
 `;
+
+// SearchBox
+export const SearchBoxContainer = styled.table`
+   width: 100%;
+   th,
+   td {
+      text-align: center;
+      vertical-align: middle;
+      padding: 10px 20px;
+      box-sizing: border-box;
+   }
+   td {
+      border-bottom: 1px solid #dcdcdc;
+      &.notice {
+         &:nth-child(3) {
+            text-align-last: left;
+         }
+      }
+      &.qna {
+         &:nth-child(2),
+         &:nth-child(3) {
+            text-align-last: left;
+         }
+      }
+   }
+`;
